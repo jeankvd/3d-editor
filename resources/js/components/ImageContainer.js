2eb59b01cbce4852cffffd0bb1be30a950b6bfe2
Jean <djjean85@gmail.com>
2019-01-03 20:26:20 -0500

    initial commit

--- resources/js/components/ImageContainer.js
+++ resources/js/components/ImageContainer.js
@@ -9,7 +9,7 @@ const Wrap = styled.div`
     margin: 0 auto;
 `;
 
-const Depth = new PIXI.Sprite.fromImage("http://i.imgur.com/tZsAgFP.jpg");
+const Depth = new PIXI.Sprite.fromImage("/images/depth.jpeg");
 let f = new PIXI.filters.DisplacementFilter(Depth, 1);
 // const displacement = new PIXI.filters.DisplacementFilter(Depth, 0);
 
@@ -18,7 +18,9 @@ class ImageContainer extends Component {
         super(props);
         this.state = {
             mouseY: 0,
-            mouseX: 0
+            mouseX: 0,
+            image: "",
+            forceCanvas: false
         };   
     }
 
@@ -37,20 +39,35 @@ class ImageContainer extends Component {
     handleMouseLeave = (e) => {
         f.scale.x = 1;
         f.scale.y = 1;
+        this.setState({ image : "" });
+    }
+
+    /* Save Canvas as an Image */
+    handleClick = () => {
+        this.setState({ forceCanvas: true  }, () => {
+            let canvasSave = document.getElementById('resetCanvas');
+            canvasSave.getContext('webgl', {preserveDrawingBuffer: false});
+            let d = canvasSave.toDataURL('image/png', 1);
+            // const w = window.open('about:blank', 'image from canvas');
+            // w.document.write("<img src='"+d+"' alt='from canvas'/>");
+            this.setState({ image: d });
+        });
     }
 
     render() {
         return (
-            <Stage width={400} height={400} onMount={this.onMount} onMouseMove={this.handleMouseMove} onMouseLeave={this.handleMouseLeave}>
+            <div>
+            <Stage id="resetCanvas" options={{preserveDrawingBuffer: true}}  width={400} height={400} onMount={this.onMount} onMouseMove={this.handleMouseMove} onMouseLeave={this.handleMouseLeave} onClick={this.handleClick}>
                 <Container position={[0,0]}>
                     {/* Foreground */}
-                    <Sprite filters={[f]}  image="http://i.imgur.com/n0WMktI.jpg" />
+                    <Sprite filters={[f]}  image="/images/test.jpg" />
                     {/* Background (ehh, not sure if necessary tbh)  */}
-                    <Sprite image={"http://i.imgur.com/tZsAgFP.jpg"} renderable={false} />
                     {/* Addition */}
                     <Sprite width="200" height="200" image={"https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"} />
                 </Container>
             </Stage>
+            <img src={this.state.image} alt=""/>
+            </div>
         );
     }
 }
